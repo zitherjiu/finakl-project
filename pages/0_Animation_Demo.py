5254bdd09a860e1aaf1ed9978f14e4f3b890c93d
zitherjiu <145343542+zitherjiu@users.noreply.github.com>
2023-11-10 10:49:30 +0800

    Description

--- pages/0_Animation_Demo.py
+++ pages/0_Animation_Demo.py
@@ -12,73 +12,55 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import Any
-
-import numpy as np
-
+#from io import BytesIO
 import streamlit as st
-from streamlit.hello.utils import show_code
+import pandas as pd
+#from matplotlib import pyplot as plt
+import seaborn as sns
+#import numpy as np
+#import requests
 
 
-def animation_demo() -> None:
-
-    # Interactive Streamlit elements, like these sliders, return their value.
-    # This gives you an extremely simple interaction model.
-    iterations = st.sidebar.slider("Level of detail", 2, 20, 10, 1)
-    separation = st.sidebar.slider("Separation", 0.7, 2.0, 0.7885)
-
-    # Non-interactive elements return a placeholder to their location
-    # in the app. Here we're storing progress_bar to update it later.
-    progress_bar = st.sidebar.progress(0)
-
-    # These two elements will be filled in later, so we create a placeholder
-    # for them using st.empty()
-    frame_text = st.sidebar.empty()
-    image = st.empty()
-
-    m, n, s = 960, 640, 400
-    x = np.linspace(-m / s, m / s, num=m).reshape((1, m))
-    y = np.linspace(-n / s, n / s, num=n).reshape((n, 1))
-
-    for frame_num, a in enumerate(np.linspace(0.0, 4 * np.pi, 100)):
-        # Here were setting value for these two elements.
-        progress_bar.progress(frame_num)
-        frame_text.text("Frame %i/100" % (frame_num + 1))
-
-        # Performing some fractal wizardry.
-        c = separation * np.exp(1j * a)
-        Z = np.tile(x, (n, 1)) + 1j * np.tile(y, (1, m))
-        C = np.full((n, m), c)
-        M: Any = np.full((n, m), True, dtype=bool)
-        N = np.zeros((n, m))
-
-        for i in range(iterations):
-            Z[M] = Z[M] * Z[M] + C[M]
-            M[np.abs(Z) > 2] = False
-            N[M] = i
-
-        # Update the image placeholder by calling the image() function on it.
-        image.image(1.0 - (N / N.max()), use_column_width=True)
-
-    # We clear elements by calling empty on them.
-    progress_bar.empty()
-    frame_text.empty()
-
-    # Streamlit widgets automatically run the script from top to bottom. Since
-    # this button is not connected to any other logic, it just causes a plain
-    # rerun.
-    st.button("Re-run")
 
 
-st.set_page_config(page_title="Animation Demo", page_icon="📹")
-st.markdown("# Animation Demo")
-st.sidebar.header("Animation Demo")
-st.write(
-    """This app shows how you can use Streamlit to build cool animations.
-It displays an animated fractal based on the the Julia Set. Use the slider
-to tune different parameters."""
-)
+df = pd.read_csv("D:\MISY\Homework\Assignment\\Cleaned_DS_Jobs.csv")
 
-animation_demo()
 
-show_code(animation_demo)
+
+# 设置网页标题
+st.title('Final Project:  Analysis based on dataset Data Science Job Posting on Glassdoor')
+
+col1, col2,col3 = st.columns([10,20,10]) 
+with col1:
+    st.subheader('Things about the dataset:')
+    button_click = st.button('Click Me')
+    if button_click:
+       st.write("This is a dataset of data science job posts in glassdoor and the data inside was scrapped from glassdoor's website.")
+       st.write("There are two versions of the data, one is uncleaned and another one is cleaned and the version we downloaded is a cleaned version. Web scrapping, Data cleaning & EDA code are added in the code section.")
+with col3:
+    st.subheader('Things about the app:')
+    button_click = st.button('Click Here')
+    if button_click:
+        st.write("""Our app is mainly focus on the connections between different variables in the dataset. Our app can be divided into three major parts.""") 
+                    
+        st.write("""In the first part, We focus our analysis on counting and comparing the number of Different Job Types and Different Onerships in Data Science, mainly using bar charts to display the data.""")
+                    
+        st.write("""In the second part, we analyze the salary and its related influencing factors. In this part, we use bar chart and heatmap to show data and correlation between variables.""")
+                    
+        st.write("""In the thrid part, we analyzed the relationship between ranking and company age and size. Since we noticed some data on skills and our major is information management, we also do some research on different programming skills which surprisingly shows that python is an very useful skill.""")
+
+with col2:
+    url = "https://img0.baidu.com/it/u=125858522,2865179145&fm=253&fmt=auto&app=138&f=JPEG?w=777&h=500"
+    st.image(url)
+    
+
+#col3,col4 = st.columns([30,30])
+
+#col5,col7,col6 = st.columns([10,20,10])
+
+
+#with col6:
+    button_click = st.button('ReadMe')
+    if button_click:
+        st.write("""For the final project, we downloaded a clean_Data Science_job table for the recommend webiste(www.kaggle.com) and the format of this table is .csv
+                There are 660 data in this table. Since it's a cleaned dataset, it contains 10 variables, which are: Job Title, Salary Estimate, Job Description, Rating, Company Name, Location, Headquarters, Size, Type of ownership and Industry""")
